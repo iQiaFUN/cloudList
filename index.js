982872c29d334cee20dd8775869720957caeff7a
iQiaFUN <github@iqia.fun>
2022-05-20 10:35:39 +0800

    update

--- index.js
+++ index.js
@@ -5,40 +5,135 @@ const token = cfg.token;
 const server = cfg.server;
 const uuid = cfg.uuid;
 const baseUrl = 'https://api.blackbe.xyz/openapi/v3';
-const cmd = cfg.cmd;
+const vcfg =  NIL._vanilla.cfg;
 
 
-function onStart(api){
-    api.listen('onMainMessageReceived',(e)=>{
-        let t = getText(e);
-		if(t == cmd){
-			if(NIL._vanilla.wl_exists(e.sender.qq)){
-				//copy于vanilla/onMain.js 214行
-                let qq = e.sender.qq;
-				let xbox = NIL._vanilla.get_xboxid(qq);
-                getData(xbox,qq).then((dt)=>{
-                    uploadList(dt).then((response)=>{
-                        let data = response.data;
-                        if(data.status == 2000){
-                            e.reply(data.message)
+class CloudList extends NIL.ModuleBase{
+    onStart(api){
+        api.listen('onMainMessageReceived',(e)=>{
+            let t = getText(e);
+            let pt = t.split(' ');
+            if(cfg.auto_CL && t == cfg.autoCMD){
+                if(NIL._vanilla.wl_exists(e.sender.qq)){
+                    let qq = e.sender.qq;
+                    let xbox = NIL._vanilla.get_xboxid(qq);
+                    checkPrivate(xbox,qq).then((r)=>{
+                        if(r == false){
+                            getData(xbox,qq).then((dt)=>{
+                                uploadList(dt).then((response)=>{
+                                    let data = response.data;
+                                    if(data.status == 2000){
+                                        e.reply(data.message)
+                                    }else{
+                                        e.reply(`上传失败\n原因${data.message}`)
+                                    }     
+                                }).catch((err)=>{
+                                    if(err.response.status==403){
+                                        e.reply(err.response.data.message)
+                                    }
+                                });
+                            }).catch((err)=>{
+                                console.log(err)
+                            });
                         }else{
-                            e.reply(`上传失败\n原因${data.message}`)
-                        }     
-                    }).catch((err)=>{
-                        if(err.response.status==403){
-                            e.reply(err.response.data.message)
+                            e.reply('玩家已存在于云端白名单中')
+                        }
+                    })             
+                }else{
+                    e.reply('你还没绑定xboxid!',true);
+                }
+            }
+            switch(pt[0]){
+                case cfg.upload:
+                    if (NIL._vanilla.isAdmin(e.sender.qq) == false) {
+                        e.reply(`你不是管理员，无法进行此操作！`);
+                        return;
+                    }
+                    var at = getAt(e);
+                    if (e.length != 0) {
+                        at.forEach(element => {
+                            if (NIL._vanilla.wl_exists(element)) {
+                                let xbox = NIL._vanilla.get_xboxid(element);
+                                checkPrivate(xbox,element).then((r)=>{
+                                    if(r == false){
+                                        getData(xbox,element).then((dt)=>{
+                                            uploadList(dt).then((response)=>{
+                                                let data = response.data;
+                                                if(data.status == 2000){
+                                                    e.reply(data.message)
+                                                }else{
+                                                    e.reply(`上传失败\n原因${data.message}`)
+                                                }     
+                                            }).catch((err)=>{
+                                                if(err.response.status==403){
+                                                    e.reply(err.response.data.message)
+                                                }
+                                            });
+                                        }).catch((err)=>{
+                                            console.log(err)
+                                        });
+                                    }else{
+                                        e.reply('玩家已存在于云端白名单中')
+                                    }
+                                })
+                            } else {
+                                e.reply(`成员${element}还没有绑定白名单！`);
+                            }
+                        });
+                    }
+                    break
+                case cfg.delete:
+                    if (NIL._vanilla.isAdmin(e.sender.qq) == false) {
+                        e.reply(`你不是管理员，无法进行此操作！`);
+                        return;
+                    }
+                    var at = getAt(e);
+                    at.forEach(element => {
+                        if (!NIL._vanilla.wl_exists(element)) {
+                            e.reply(`成员${element}还没有绑定白名单！`);
+                        } else {
+                            let xbox = NIL._vanilla.get_xboxid(element);
+                            checkPrivate(xbox,element).then((r)=>{
+                                if(r !== false){
+                                    for(let i = 0 ; i < r.length ; i++){
+                                        deletePrivate(r[i]).then((response)=>{
+                                            let data = response.data;
+                                            if(data.status == 2000){
+                                                e.reply(`${r[i]}\n${data.message}`)
+                                            }else{
+                                                e.reply(`删除失败\n原因${data.message}`)
+                                            }
+                                        }).catch((err)=>{
+                                            if(err.response.status==403){
+                                                e.reply(err.response.data.message)
+                                            }
+                                        });
+                                    }    
+                                }
+                            })
                         }
                     });
-                }).catch((err)=>{
-                    console.log(err)
-                });
-			}else{
-				e.reply('你还没绑定xboxid!',true);
-			}
-		}
-    })
+                    break
+                default:
+                    return
+            }
+        })
+        if(cfg.auto_DEL){
+            api.listen('onGroupMemberLeft',onLeft);
+        }
+        if (cfg.auto_UP) {
+            api.listen('onMemberBinding',onBind);
+        }
+        api.listen('onMemberUnBinding',onUnBind);
+    }
+
+    onStop(){}
 }
 
+
+/**
+ * 获取聊天文本
+ */
 function getText(e) {
     var rt = '';
     for (i in e.message) {
@@ -51,6 +146,92 @@ function getText(e) {
     return rt;
 }
 
+function onBind(e){
+    let xbox = e.xboxid;
+    let qq = e.member.qq;
+    //console.log(`${xbox} ${qq}`);
+    checkPrivate(xbox,qq).then((r)=>{
+        if(r == false){
+            getData(xbox,qq).then((dt)=>{
+                uploadList(dt).then((response)=>{
+                    let data = response.data;
+                    if(data.status == 2000){
+                        NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`${data.message}`);
+                    }else{
+                        NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`上传失败\n原因${data.message}`);
+                    }     
+                }).catch((err)=>{
+                    if(err.response.status==403){
+                        NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`${err.response.data.message}`);
+                    }
+                });
+            }).catch((err)=>{
+                console.log(err);
+            });
+        }else{
+            NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`玩家已存在于云端白名单中`);
+        }
+    })
+}
+
+function onLeft(e){
+    if(e.group_id == vcfg.group.main && e.self_id == vcfg.self_id){
+        let xbox = '';
+        let qq = e.user_id;
+        removeCloudList(xbox,qq);
+    }
+}
+
+function onUnBind(e){
+    let xbox = e.xboxid;
+    let qq = e.member.qq;
+    removeCloudList(xbox,qq);
+}
+
+function removeCloudList(xbox,qq){
+    checkPrivate(xbox,qq).then((r)=>{
+        if(r !== false){
+            for(let i = 0 ; i < r.length ; i++){
+                deletePrivate(r[i]).then((response)=>{
+                    let data = response.data;
+                    if(data.status == 2000){
+                        NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`${r[i]}\n${data.message}`)
+                    }else{
+                        NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`删除失败\n原因${data.message}`)
+                    }
+                }).catch((err)=>{
+                    if(err.response.status==403){
+                        NIL.bots.getBot(vcfg.self_id).sendGroupMsg(vcfg.group.main,`${err.response.data.message}`)
+                    }
+                });
+            }    
+        }
+    })
+}
+/**
+ * getAt
+ * @param {*} 
+ * @returns 
+ */
+function getAt(e) {
+    var at = [];
+    for (i in e.message) {
+        switch (e.message[i].type) {
+            case "at":
+                at.push(e.message[i].qq);
+                break;
+        }
+    }
+    return at;
+};
+
+/**
+ * 输出data
+ * @param {*} name 
+ * @param {*} qq 
+ * @param {*} phone 
+ * @returns Promise对象
+ */
 function getData(name,qq,phone = 10000000000){
     const date = new Date();
     let Xuid = getXuid(name).then((res)=>{
@@ -81,6 +262,9 @@ function getData(name,qq,phone = 10000000000){
     return data
 }
 
+/**
+ * 格式化时间
+ */
 function DateFormat(date){
 	let d = date;
 	let YYYY = d.getFullYear()
@@ -90,6 +274,11 @@ function DateFormat(date){
 	return f;
 }
 
+/**
+ * 数字补零
+ * @param {*} a 
+ * @returns 
+ */
 function numFormat(a){
 	let b = 0;
 	if(a < 10){
@@ -100,6 +289,11 @@ function numFormat(a){
 	return b;
 }
 
+/**
+ * Post 云端白名单
+ * @param {*} data 
+ * @returns 
+ */
 function uploadList(data){
     Author = `Bearer ${token}`;
     let r = axios(
@@ -115,9 +309,91 @@ function uploadList(data){
     return r
 }
 
+function deletePrivate(uuid){
+    Author = `Bearer ${token}`;
+    let r = axios(
+        {
+            method: "POST",
+            headers: {
+                "Authorization":Author
+            },
+            url:`${baseUrl}/private/repositories/piece/delete`,
+            data: {
+                piece_uuid: uuid
+            }
+        }
+    )
+    return r
+}
 
 
+/**
+ * 检测私有库
+ * @param {*} id 
+ * @returns 
+ */
+function checkPrivate(name,qq){
+    Author = `Bearer ${token}`;
+    let Xuid = getXuid(name).then((res)=>{
+        let xuid = res;
+        console.log(xuid);
+        return xuid
+    }).catch((err)=>{
+        console.log(err)
+    });
+    let r = Xuid.then((xuid)=>{
+        
+        return axios(
+            {
+                method: "POST",
+                headers: {
+                    "Authorization":Author
+                },
+                url:`${baseUrl}/check/private?name=${name}&qq=${qq}&xuid=${xuid}`,
+                data: { repositories_uuid: [ uuid ] }
+            }
+        ).then((response)=>{
+            const data = response.data;
+            if (data.status == 2000) {
+                let dt = data.data[0];
+                if(dt.repo_success){
+                    if(dt.exist){
+                        let info = dt.info;
+                        let tmp = [];
+                        for(let i = 0 ; i < info.length;i++){
+                            
+                            tmp[i] = info[i].uuid;
+                        }
+                        console.log(tmp);
+                        return tmp;
+                    }else{
+                        console.log('用户不在该云黑库');
+                        return false
+                    }
+                }else{
+                    console.log('查询仓库失败');
+                    return false
+                }
+            } else {
+                console.error(data.message);
+                return false
+            }
+        })
+    }).catch((err)=>{
+        console.log(err)
+    });
+    return r
+}
+
+/**
+ * 获取xuid
+ * @param {*} id 
+ * @returns 
+ */
 function getXuid(id){
+    if(id == ''){
+        id = 'blackbetest'
+    }
     let r = new Promise((resolve,reject)=>{
         axios.get(`${baseUrl}/utils/xuid?gamertag=${id}`)
         .then((response)=>{
@@ -137,7 +413,4 @@ function getXuid(id){
     return r
 }
 
-module.exports = {
-    onStart,
-    onStop(){}
-};
+module.exports = new CloudList;
