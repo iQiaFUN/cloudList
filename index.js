00bf1ac9fbfe7ff05828795cebc5a0faca4ba005
iQiaFUN <github@iqia.fun>
2022-05-20 14:09:22 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const axios = require('axios');
 const path = require('path');
+const tmpcfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'example.json')));
+
+function checkFile(file, text) {
+    if (NIL.IO.exists(path.join(__dirname, file)) == false) {
+        NIL.IO.WriteTo(path.join(__dirname, file), text);
+    }
+}
+
+checkFile("config.json", tmpcfg);
 const cfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'config.json')));
 const token = cfg.token;
 const server = cfg.server;
